767b8c33ccbc86874d895e32a49d293f19d02ce6
uknowkumar <uknow.kumar@gmail.com>
2025-10-29 18:12:37 +0530

    Simplify console logs in onSave and onChange methods
    Reduced console log statements for data save and change events.

--- src/app/data-binding/data-binding.ts
+++ src/app/data-binding/data-binding.ts
@@ -14,20 +14,12 @@ export class DataBinding {
   // pic : string = "https://i.imgur.com/OB0y6MR.jpg";
   random : string = "";
   onSave(){
-   console.log("Data Saved Successfully.. Testing conflicts..");
-    console.log("Data Saved Successfully.. Testing conflicts..");
-    console.log("Testing conflicts.. Data Saved Successfully.. ");
-    console.log("Data Saved Successfully.. Testing conflicts..");
+   console.log("Data Save");
     alert("Data Saved Successfully..");
-    console.log("Data Saved Successfully.. Testing conflicts..");
   }
   onChange(){
-   console.log("Country has Changed.. Testing conflicts..");
     console.log("Country has Changed.. Testing conflicts..");
-    console.log("Testing conflicts.. Country has Changed..");
     alert("Country has Changed..");
-    console.log("Country has Changed.. Testing conflicts..");
-
-  }
+     }
 }
 
