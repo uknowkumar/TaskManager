c72f0c0053169572a58d2ed8599a28bca0dbbd87
uknowkumar <uknow.kumar@gmail.com>
2025-10-29 18:22:08 +0530

    Add local variables in onSave and onChange methods
    Added local variables name1, topic1, name2, and topic2 in onSave and onChange methods.

--- src/app/data-binding/data-binding.ts
+++ src/app/data-binding/data-binding.ts
@@ -14,13 +14,16 @@ export class DataBinding {
   // pic : string = "https://i.imgur.com/OB0y6MR.jpg";
   random : string = "";
   onSave(){
+     name1 : string = "FEDLearning";
+    topic1 : string = "data binding";
     console.log("Data Save");
     alert("Data Saved Successfully..");
     console.log("Data Save");
   }
 
   onChange(){
-    console.log("Country has Changed..");
+    name2 : string = "FEDLearning";
+    topic2 : string = "data binding";console.log("Country has Changed..");
     alert("Country has Changed..");
     console.log("Country has Changed..");
   }
